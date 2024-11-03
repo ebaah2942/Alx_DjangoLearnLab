e6f0955d53242209b4076ea0fd0bbb9f4501cb33
Enoch Baah <ebaah2942@gmail.com>
2024-11-03 13:17:38 +0000

    Add new files

--- Introduction_to_Django/LibraryProject/bookshelf/models.py
+++ Introduction_to_Django/LibraryProject/bookshelf/models.py
@@ -6,3 +6,6 @@ class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
     publication_year = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.title} by {self.author} published in {self.publication_year}"
