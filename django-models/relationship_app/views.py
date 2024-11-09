3f93615b30fcd60c8976aa79c5f808929a40f206
Enoch Baah <ebaah2942@gmail.com>
2024-11-09 13:28:47 +0000

    new changes

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -11,7 +11,7 @@ def book_list(request):
 
 
 class BookListView(DetailView):
-    model = Book
+    model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
 
