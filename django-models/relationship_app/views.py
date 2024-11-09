c4da79ebbac1ae9fb5f4462d54ad71aacea1e134
Enoch Baah <ebaah2942@gmail.com>
2024-11-09 13:29:50 +0000

    new changes

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from . models import Book
 from django.views.generic import DetailView
-from . models import Library
+from .models import Library
 
 # Create your views here.
 
