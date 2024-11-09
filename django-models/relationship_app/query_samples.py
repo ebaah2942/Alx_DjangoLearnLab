4185ec05e7680df89de02b2d8537fed74251e127
Enoch Baah <ebaah2942@gmail.com>
2024-11-09 10:53:29 +0000

    Add new files

---
+++ django-models/relationship_app/query_samples.py
@@ -0,0 +1,15 @@
+from relationship_app.models import *
+
+def query():
+    
+    author = Author.objects.get(name="J.K. Rowling")
+
+
+def list_books():
+    books = Book.objects.all()
+    return books
+
+
+def retrieve_librarian():
+    librarian = Librarian.objects.get(name="Enoch Baah")
+    return librarian
