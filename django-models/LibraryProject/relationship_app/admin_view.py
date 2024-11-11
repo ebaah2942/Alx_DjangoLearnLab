89a1c91c7bf1100bbf574d7b6cc9655d9427114b
Enoch Baah <ebaah2942@gmail.com>
2024-11-11 18:16:42 +0000

    New files

--- django-models/LibraryProject/relationship_app/admin_view.py
+++ django-models/LibraryProject/relationship_app/admin_view.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from .models import UserProfile
 
 def is_admin(user):
-    return user.userprofile.role == 'Admin'
+    return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
 
 
 @user_passes_test(is_admin)
