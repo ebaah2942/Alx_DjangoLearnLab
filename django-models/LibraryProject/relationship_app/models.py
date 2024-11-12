977cba8d270fc5ba934507be8c56b91d6ff9e529
Enoch Baah <ebaah2942@gmail.com>
2024-11-12 01:08:41 +0000

    New files

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -19,10 +19,11 @@ class Book(models.Model):
     
     class Meta:
         permissions = [
-            ["Can_add_book"],
-            ["can_change_book"],
-            ["can_delete_book"],
-        ]
+            ("can_add_book", "Can add a book"),
+            ("can_change_book", "Can change a book"),
+            ("can_delete_book", "Can delete a book"),
+
+        ]    
     
 
 
