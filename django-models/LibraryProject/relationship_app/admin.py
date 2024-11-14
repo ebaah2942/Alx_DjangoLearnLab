0df22df035008fa62827cb05b7b1552f888a649c
Enoch Baah <ebaah2942@gmail.com>
2024-11-14 07:12:49 +0000

    New files

--- django-models/LibraryProject/relationship_app/admin.py
+++ django-models/LibraryProject/relationship_app/admin.py
@@ -10,10 +10,11 @@ from django.http import HttpResponse
 
 
 def is_admin(user):
-    return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
+    return user.is_superuser
+    # return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
     # return user.userprofile.role == 'Admin'
 
-@user_passes_test(is_admin)
+@user_passes_test(is_admin, login_url='/login/')
 def admin_view(request):
     return render(request, 'relationship_app/admin_view.html')
     # return HttpResponse("Welcome, Admin!")
