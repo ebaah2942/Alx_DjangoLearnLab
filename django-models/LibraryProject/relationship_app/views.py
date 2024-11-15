3fd82e3c3cd3ef41b7b0f783b7371c8ae34ede3b
Enoch Baah <ebaah2942@gmail.com>
2024-11-15 08:42:34 +0000

    New files

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -10,6 +10,7 @@ from django.shortcuts import redirect, render, get_object_or_404
 from .forms import BookForm
 from django.contrib.auth.decorators import permission_required
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import user_passes_test
 # Create your views here.
 
 def list_books(request):
@@ -74,4 +75,29 @@ def delete_book(request, pk):
 @login_required
 def member_view(request):
     # Your view logic here
+    return render(request, 'relationship_app/member_view.html')
+
+
+def is_librarian(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian'
+
+def is_member(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
+
+def is_admin(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
+
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+
+@user_passes_test(is_member)
+def member_view(request):
     return render(request, 'relationship_app/member_view.html')
