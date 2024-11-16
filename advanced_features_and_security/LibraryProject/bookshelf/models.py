c8523cafd4f5e70098319c680b7636f5c62095f3
Enoch Baah <ebaah2942@gmail.com>
2024-11-16 13:15:33 +0000

    Added new files

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -44,9 +44,6 @@ class CustomUserAdmin(BaseUserManager):
     
 
 
-
-    from django.db import models
-
 class Article(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseForbidden
 from django.contrib.auth.decorators import login_required
 from .models import Article
+from django.contrib.auth.decorators import permission_required
+from .forms import ArticleForm
+from django.shortcuts import render, redirect
 
 @login_required
 def view_article(request, article_id):
@@ -10,3 +13,24 @@ def view_article(request, article_id):
         return HttpResponseForbidden("You do not have permission to view this article.")
     article = get_object_or_404(Article, id=article_id)
     return render(request, 'article_detail.html', {'article': article})
+
+
+
+@permission_required('app_name.can_create', raise_exception=True)
+def create_article(request):
+    if request.method == 'POST':
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('article_list')  # Redirect to article list or success page
+    else:
+        form = ArticleForm()
+    return render(request, 'create_article.html', {'form': form})
+
+@permission_required('app_name.can_delete', raise_exception=True)
+def delete_article(request, article_id):
+    article = get_object_or_404(Article, id=article_id)
+    if request.method == 'POST':
+        article.delete()
+        return redirect('article_list')  # Redirect to article list after deletion
+    return render(request, 'delete_article.html', {'article': article})
