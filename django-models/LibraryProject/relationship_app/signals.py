72a4ce6d3153546d12c6d901995bc6cf42ae95c4
Enoch Baah <ebaah2942@gmail.com>
2024-11-22 11:00:04 +0000

    New changes

--- django-models/LibraryProject/relationship_app/signals.py
+++ django-models/LibraryProject/relationship_app/signals.py
@@ -2,8 +2,10 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth.models import User
 from .models import UserProfile
+from django.db import models
 
-
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
