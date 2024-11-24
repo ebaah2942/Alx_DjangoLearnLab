7468fa45c537aa6e256af20b96286839b8af966d
Enoch Baah <ebaah2942@gmail.com>
2024-11-24 13:43:03 +0000

    Security features added

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -2,6 +2,8 @@ from django.urls import path, include
 from .views import BookList
 from .views import BookViewSet
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import obtain_auth_token
+
 
 # A default router is created to generate URL patterns for the BookViewSet
 
@@ -12,4 +14,5 @@ urlpatterns = [
     # Maps to the BookList view and the BookViewSet
     path('', include(router.urls)),
     path('books/', BookList.as_view(), name='book-list'),  # Maps to the BookList view
+    path('token/', obtain_auth_token, name='obtain_token'),
 ]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -4,6 +4,7 @@ from django.views import generic
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
 # Create your views here.
 
@@ -14,7 +15,20 @@ class BookList(generic.ListView):
     def get_queryset(self):
         return Book.objects.all()
 
-# DRF built in view to retrieve all books
+# DRF built in view to retrieve all books for authenticated users
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    # This require the user to be authenticated via token-key
+    authentication_classes = (IsAuthenticated,)
+
+# class BookListAuthenticated(ListAPIView):
+#     queryset = Book.objects.all()
+#     serializer_class = BookSerializer
+#     permission_classes = [IsAuthenticated]
+
+# This class retreives all books for admin
+class BookListAdmin(ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAdminUser]        
