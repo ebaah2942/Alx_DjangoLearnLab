8fa07d3fb9e0ebf6cb1b3478896bc421fd61751b
Enoch Baah <ebaah2942@gmail.com>
2024-11-24 13:48:23 +0000

    Security features added

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -129,6 +129,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 DEFAULT_AUTHENTICATION_CLASSES = [
     'rest_framework.authentication.TokenAuthentication',
+    'rest_framework.permissions.IsAuthenticated',
 ]
 
 
