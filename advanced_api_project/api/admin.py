78f52c9ffcafcb2068be8573215035b0c267dfce
Enoch Baah <ebaah2942@gmail.com>
2024-11-29 16:02:28 +0000

    New project

---
+++ advanced_api_project/api/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import Book, Author
+
+# Register your models here.
+
+# Register your models here.
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'author', 'publication_year']
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):    
+    list_display = ['id', 'name']
---
+++ advanced_api_project/api/serializers.py
@@ -0,0 +1,31 @@
+from rest_framework import serializers
+from .models import Author, Book
+
+# Serialized the name field from Author model
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Author
+        fields = ['name']
+    
+
+
+# Serialized the Book model for all the fields
+class BookSerializer(serializers.ModelSerializer):
+    # This dynamivally adds the related_books field and serializes it
+    related_books = serializers.SerializerMethodField()
+    class Meta:
+        model = Book
+        fields = '__all__'    
+
+    # This method serializes the related_books field
+    def get_related_bbooks(self, obj):
+        if obj.related_books.exists():
+            return BookSerializer(obj.related_books.all(), many=True).data
+        return None
+    
+    
+    # This method validates the publication year
+    def validate_publication_year(self, value):
+        if value > 2024:
+            raise serializers.ValidationError("Publication year cannot be in the future.")
+        return value 
