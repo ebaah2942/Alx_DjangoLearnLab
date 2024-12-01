8a8191a5cc0fc31d5f0b54a4dde5b0a4596db540
Enoch Baah <ebaah2942@gmail.com>
2024-12-01 11:37:42 +0000

    New features

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -6,7 +6,7 @@ from .serializers import BookSerializer
 from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView, UpdateAPIView, DestroyAPIView
 from .permissions import IsAdminOrReadOnly
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.filters import SearchFilter
+from rest_framework.filters import SearchFilter, OrderingFilter
 from django_filters import rest_framework
 from rest_framework import generics
 
@@ -17,7 +17,7 @@ class BookListView(ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
-    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     # Filter by title, author, and publication year for large datasets
     filterset_fields = ['title', 'author', 'publication_year']
     # Search by title and author for easy navigation
