aef34e9bf00653d3fba79c392679a597dc0d73f4
Enoch Baah <ebaah2942@gmail.com>
2024-12-05 01:15:21 +0000

    New features added

---
+++ django_blog/blog/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+
+class CustomUserCreationForm(UserCreationForm):
+    email = forms.EmailField(max_length=254, help_text='Required. Enter a valid email address.')
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = '__all__'        
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import CustomUserCreationForm
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from .forms import ProfileForm
+
 
 # Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('profile')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'blog/register.html', {'form': form})
+
+
+@login_required
+def profile(request):
+    return render(request, 'blog/profile.html', {'user': request.user})
+
+
+
+@login_required
+def edit_profile(request):
+    
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('profile')
+    else:
+        form = ProfileForm(instance=request.user)
+    return render(request, 'blog/edit_profile.html', {'form': form})
+
+def home_view(request):
+    return render(request, 'blog/home.html')
