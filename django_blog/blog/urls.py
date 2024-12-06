f551e985a3d3fad66be7150c1d79b814a6277d69
Enoch Baah <ebaah2942@gmail.com>
2024-12-06 17:13:06 +0000

    Added commeent feature

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -18,7 +18,7 @@ urlpatterns = [
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
     path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='comment_edit'),
     path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment_delete'),
-    path('/posts/<int:pk>/comments/new/', CommentCreateView.as_view(), name='add_comment_to_post'),
+    path('/post/<int:pk>/comments/new/', CommentCreateView.as_view(), name='add_comment_to_post'),
 
 
 
