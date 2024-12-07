cc7a54e6dde2ddecded33a1e238b6b8e8b93b84c
Enoch Baah <ebaah2942@gmail.com>
2024-12-07 18:24:23 +0000

    More functionality

--- django_blog/blog/widgets.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-class TagWidget(forms.CheckboxSelectMultiple):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.attrs['class'] = 'form-check-input', style='width: 300px;'
