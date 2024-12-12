a966f80cfdb026097e1de95df47faf45111113b7
Enoch Baah <ebaah2942@gmail.com>
2024-12-12 20:35:50 +0000

    New socialmedia_api created

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 from .models import CustomUser
+from django.contrib.auth import get_user_model
+from rest_framework.authtoken.models import Token
 
 class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -8,13 +10,14 @@ class CustomUserSerializer(serializers.ModelSerializer):
 
 
 class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
     class Meta:
         model = CustomUser
         fields = ['username', 'email', 'password']   
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
-        user = CustomUser.objects.create_user(username=validated_data['username'], email=validated_data['email'], password=validated_data['password'])
+        user = get_user_model().objects.create_user(username=validated_data['username'], email=validated_data['email'], password=validated_data['password'])
         return user     
     
 
