272044fd9c0787d3273ae7c87ed4659fc2bd108b
Enoch Baah <ebaah2942@gmail.com>
2024-12-17 00:57:11 +0000

    About to deploy on render

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*.herokuapp.com']
+ALLOWED_HOSTS = ['firstapp-iszi.onrender.com']
 
 
 # Application definition
